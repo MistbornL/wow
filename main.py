2135dfa41149cdade060d8c8a49f9c8171f84039
Lasha <50033103+MistbornL@users.noreply.github.com>
2021-10-01 03:23:48 +0400

    Update

--- main.py
+++ main.py
@@ -1,12 +1,159 @@
 heroes = {
     "Dranei":
-        ["Paladin", "Shaman", "Priest", "Warrior"],
+        [
+            {
+                "Paladin":
+                    {
+                        "Hp": 580,
+                        "Mana": 100,
+                        "level": 1
+                    }
+            },
+
+            {
+                "Shaman":
+                    {
+                        "Hp": 500,
+                        "mana": 200,
+                        "level": 1
+                    }
+            },
+
+            {
+                "Priest":
+                    {
+                        "Hp": 400,
+                        "mana": 300,
+                        "level": 1
+                    }
+            },
+
+            {
+                "Warrior":
+                    {
+                        "Hp": 500,
+                        "rage": 0,
+                        "level": 1
+                    }
+            },
+        ],
+
     "Night Elf":
-        ["Hunter", "Druid", "Priest", "Mage", "Rogue"],
+            [
+                {
+                    "Rogue":
+                        {
+                            "Hp": 480,
+                            "Energy": 100,
+                            "level": 1
+                        }
+                },
+
+                {
+                    "Hunter":
+                        {
+                            "Hp": 4800,
+                            "concentration": 100,
+                            "level": 1
+                        }
+                },
+
+                {
+                    "Priest":
+                        {
+                            "Hp": 400,
+                            "mana": 300,
+                            "level": 1
+                        }
+                },
+
+                {
+                    "Warrior":
+                        {
+                            "Hp": 500,
+                            "rage": 0,
+                            "level": 1
+                        }
+                },
+            ],
+
     "Gnome":
-        ["Mage", "Rogue", "Warlock", "Priest"],
+        [
+            {
+                "Paladin":
+                    {
+                        "Hp": 580,
+                        "Mana": 100,
+                        "level": 1
+                    }
+            },
+
+            {
+                "Shaman":
+                    {
+                        "Hp": 500,
+                        "mana": 200,
+                        "level": 1
+                    }
+            },
+
+            {
+                "Rogue":
+                    {
+                        "Hp": 400,
+                        "Energy": 100,
+                        "level": 1
+                    }
+            },
+
+            {
+                "Warrior":
+                    {
+                        "Hp": 500,
+                        "rage": 0,
+                        "level": 1
+                    }
+            },
+        ],
+
     "Human":
-        ["Paladin", "Warrior", "Mage", "Warlock", "Priest", "Rogue"]
+        [
+            {
+                "Paladin":
+                    {
+                        "Hp": 580,
+                        "Mana": 100,
+                        "level": 1
+                    }
+            },
+
+            {
+                "Hunter":
+                    {
+                        "Hp": 480,
+                        "Concentration": 100,
+                        "level": 1
+                    }
+            },
+
+            {
+                "Priest":
+                    {
+                        "Hp": 400,
+                        "mana": 300,
+                        "level": 1
+                    }
+            },
+
+            {
+                "Warrior":
+                    {
+                        "Hp": 500,
+                        "rage": 0,
+                        "level": 1
+                    }
+            },
+        ]
 }
 
 
@@ -16,23 +163,32 @@ class Player:
         self.klas = None
         self.hp = None
         self.name = input("Enter YOur Character Name >> ")
+        self.options = []
 
-        if self.race == "Night Elf":
-            self.hp = 400
+        for item in heroes[self.race]:
+            for klasebi in item.keys():
+                self.options.append(klasebi)
+
+        def choose_class(self):
             if self.race in heroes:
-                self.klas = input(f"Classes to choose: {heroes[self.race]} >> ")
-        elif self.race == "Gnome":
-            self.hp == 380
-            if self.race in heroes:
-                self.klas = input(f"Classes to choose: {heroes[self.race]} >> ")
-        elif self.race == "Human":
-            self.hp = 370
-            if self.race in heroes:
-                self.klas = input(f"Classes to choose: {heroes[self.race]} >> ")
-        elif self.race == "Dranei":
-            self.hp = 420
-            if self.race in heroes:
-                self.klas = input(f"Classes to choose: {heroes[self.race]} >> ")
+                self.klas = input(f"Classes to choose: {self.options} >> ")
+                for hero in heroes[self.race]:
+                    self.hp = hero[self.klas]["Hp"]
+
+        try:
+            if self.race == "Night Elf":
+                choose_class(self)
+
+            elif self.race == "Gnome":
+                choose_class(self)
+
+            elif self.race == "Human":
+                choose_class(self)
+
+            elif self.race == "Dranei":
+                choose_class(self)
+        except KeyError:
+            pass
 
     def info(self):
         print(f"Race: {self.race}, HP: {self.hp}, Class: {self.klas}, Name: {self.name}")
@@ -42,4 +198,3 @@ p1 = Player()
 
 
 p1.info()
-

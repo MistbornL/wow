536bdc047bd42794d4ec0b7fe4918bc432e9d9a2
Dev1 <dev1@Dev1.localdomain>
2022-01-28 14:30:06 +0400

    update

--- Service/player_class.py
+++ Service/player_class.py
@@ -1,26 +1,25 @@
 from Service.heroes import heroes, spellBook
 
+
 class Player:
     def __init__(self):
         self.race = input("Choose Race: Night Elf, Gnome, Human, Dranei >> ")
         self.klas = None
         self.name = input("Enter YOur Character Name >> ")
         self.options = []
-        self.stats = []
+        self.stats = None
 
         for hero in heroes[self.race]:
             for klasebi in hero.keys():
                 self.options.append(klasebi)
 
-        for hero in heroes[self.race]:
-            try:
-                self.stats.append(hero[self.klas])
-            except KeyError:
-                pass
-
-    def choose_class_for_race(self, ):
+    def choose_class_for_race(self):
         if self.race in heroes:
             self.klas = input(f"Classes to choose: {self.options} >> ")
+            for classes in heroes[self.race]:
+                for item in classes:
+                    if item == self.klas:
+                        self.stats = classes[self.klas]
 
     def info(self):
         print(f"Race: {self.race}, stats: {self.stats}, Class: {self.klas}, Name: {self.name}")
